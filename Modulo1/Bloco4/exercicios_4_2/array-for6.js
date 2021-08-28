c04ec2ef75fff1f8ce2cbde3a6faf97ad02a1799
Thauler <thauler98@gmail.com>
2021-08-28 15:21:15 -0300

    Change 6 to 7 and finishes 6

--- Modulo1/Bloco4/exercicios_4_2/array-for6.js
+++ Modulo1/Bloco4/exercicios_4_2/array-for6.js
@@ -1,12 +1,17 @@
 // Declarando um array
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// Declarar  a variavel menor numero contido no array
-let lowNumber = numbers[0];
-// Percorrer o array
-for (let index = 1; index < numbers.length; index += 1) {
-    // Criar uma condição para que ao percorrer encontre o menor numero
-    if (numbers[index] < lowNumber) {
-        lowNumber = numbers[index]
+// Declarando variavel para a quantidade de numeros impares
+let quantOdd = 0;
+// Criar um loop para percorrer o array
+for (let index = 0; index < numbers.length; index++) {
+// Cria uma condição para verificar os numeros impares
+    if (numbers[index] % 2 !== 0) {
+        quantOdd += 1;
     }
 }
-console.log(lowNumber);
+// Criar a condição caso não exista numeros impares
+if (quantOdd === 0) {
+    console.log('nenhum valor impar encontrado');
+} else {
+    console.log(quantOdd);
+}
---
+++ Modulo1/Bloco4/exercicios_4_2/array-for7.js
@@ -0,0 +1,12 @@
+// Declarando um array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// Declarar  a variavel menor numero
+let lowNumber = numbers[0];
+// Percorrer o array
+for (let index = 1; index < numbers.length; index += 1) {
+    // Criar uma condição para que ao percorrer encontre o menor numero
+    if (numbers[index] < lowNumber) {
+        lowNumber = numbers[index]
+    }
+}
+console.log(lowNumber);
