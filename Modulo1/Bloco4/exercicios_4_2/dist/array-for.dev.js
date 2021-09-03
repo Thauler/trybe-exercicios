8af21b89e0795baede6a3613b09cb1e760897265
Thauler <75540343+Thauler@users.noreply.github.com>
2021-09-03 18:46:21 -0300

    Delete Modulo1/Bloco4/exercicios_4_2/dist directory

--- Modulo1/Bloco4/exercicios_4_2/dist/array-for.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-for (var _i = 0, _numbers = numbers; _i < _numbers.length; _i++) {
-  var number = _numbers[_i];
-  console.log(number);
-}
