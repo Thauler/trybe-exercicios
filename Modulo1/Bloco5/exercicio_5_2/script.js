75f0467fec39873544b5988da80b56a0cea9f1db
Thauler <thauler98@gmail.com>
2021-09-03 19:18:57 -0300

    Task 2

--- Modulo1/Bloco5/exercicio_5_2/script.js
+++ Modulo1/Bloco5/exercicio_5_2/script.js
@@ -1,3 +1,9 @@
+// 1
 const h1 = document.createElement('h1');
 h1.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(h1);
+// 2
+const main = document.createElement('main');
+main.className = 'main-content';
+document.body.appendChild(main);
+// 3
