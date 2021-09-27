8769afb6832a0df6b202dd036919f4118ff76f4c
Thauler <thauler98@gmail.com>
2021-09-27 13:55:39 -0300

    Parte 1 do 7.1

---
+++ Modulo1/Bloco7/exercicios_7_1/parte1/es6.js
@@ -0,0 +1,17 @@
+function testingScope(escopo) {
+    if (escopo === true) {
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+        console.log(ifScope);
+    } else {
+        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+    }
+}
+
+testingScope(true);
+
+// Ordenando os numeros na ordem crescente
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort(function(a, b) { return a - b });
+console.log(oddsAndEvens);
