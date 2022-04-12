35640bdb5870127691321525dd242d59fa5aaa80
Thauler <thauler98@gmail.com>
2022-04-11 21:30:36 -0300

    setup inicial do exercicio de ORM

---
+++ back-end/Bloco24/exercicios_24_1/migrations/20220411224207-create-books.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+      await queryInterface.dropTable('Books');
+  }
+};
